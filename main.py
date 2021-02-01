9a111e84c2cb40bca0575a771d2c502442858177
Ranie <ranie.black29@gmail.com>
2021-02-01 15:04:02 -0300

    Jogo De Jokenpo

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 from random import randint
 
-print('Vamos Jogar Jokenpo')
+print('Vamos Jogar Jokenpo\n 0 = Sim\n 1 = Nao')
 print(' ')
-print('Faça Sua Escolha\n 0 = Papel\n 1 = Pedra\n 2 = Tesoura\n')
 
 
-jokenpo = ('Papel', 'Pedra', 'Tesoura')
-escolha = int(input('Digite sua Escolha: '))
-computador = randint(0, 2)
+jokenpo = ['Papel', 'Pedra', 'Tesoura']
+jogar = int(input('Digite sua Opçao: '))
+print('')
 
-jogo1 = jokenpo[escolha]
-jogo2 = jokenpo[computador]
+if jogar >= 3:
+    print('Opcao Invalida')
 
-print(f'Sua Escolha Foi: {jogo1}\nO Computador Escolheu: {jogo2}')
+while jogar < 1:
+    print('Faça Sua Escolha\n 0 = Papel\n 1 = Pedra\n 2 = Tesoura\n')
+    escolha = int(input('Digite sua Escolha: '))
+    computador = randint(0, 2)
 
-if jogo1 == jogo2:
-    print('Empatou')
+    jogo1 = jokenpo[escolha]
+    jogo2 = jokenpo[computador]
 
-elif escolha == 0 and computador == 1 or escolha == 1 and computador == 2 or escolha == 2 and computador == 0:
-    print('voce Ganhou')
+    print(f'Sua Escolha Foi: {jogo1}\nO Computador Escolheu: {jogo2}')
 
-elif computador == 0 and escolha == 1 or computador == 1 and escolha == 2 or computador == 2 and escolha == 0:
-    print('voce Perdeu')
+    if jogo1 == jogo2:
+        print('*' * 7)
+        print('Empatou')
+        print('*' * 7)
+        print('')
+
+    elif escolha == 0 and computador == 1 or escolha == 1 and computador == 2 or escolha == 2 and computador == 0:
+        print('*' * 11)
+        print('Voce Ganhou')
+        print('*' * 11)
+        print('')
+
+    elif computador == 0 and escolha == 1 or computador == 1 and escolha == 2 or computador == 2 and escolha == 0:
+        print('*' * 11)
+        print('Voce Perdeu')
+        print('*' * 11)
+        print('')
+
+    print('Que Jogar mais\n 0 = Sim\n 1 = Nao')
+    print(' ')
+    jogar = int(input('Digite sua Opçao: '))
+    print('')
+
+    if jogar >= 3:
+        print('Opcao Invalida')
+
+else:
+    print('Fim De Jogo')

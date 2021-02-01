ff77c2ff1bbb36d5e36b2c1f7048140107875dcb
Ranie <ranie.black29@gmail.com>
2021-02-01 02:34:58 -0300

    Jogo De Jokenpo

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ from random import randint
 
 print('Vamos Jogar Jokenpo')
 print(' ')
-print('Faça Sua Escolha\n 0 Papel\n 1 Pedra\n 2 Tesoura\n')
+print('Faça Sua Escolha\n 0 = Papel\n 1 = Pedra\n 2 = Tesoura\n')
 
 
-jokenpo = ['Papel', 'Pedra', 'Tesoura']
+jokenpo = ('Papel', 'Pedra', 'Tesoura')
 escolha = int(input('Digite sua Escolha: '))
 computador = randint(0, 2)
 
 jogo1 = jokenpo[escolha]
 jogo2 = jokenpo[computador]
 
-if escolha < 3:
-    print(f'Sua Escolha Foi: {jogo1}\nO Computador Escolheu: {jogo2}')
-    if jogo1 == jogo2:
-        print('Empatou')
+print(f'Sua Escolha Foi: {jogo1}\nO Computador Escolheu: {jogo2}')
 
-    # preciso saber como fazer a parte de perder e ganhar
-    elif jogo1[0] <= jogo2[1] or jogo1[1] <= jogo2[2] or jogo1[2] >= jogo2[0]:
-        print('voce Ganhou')
-    elif jogo1[1] >= jogo2[0] or jogo1[2] >= jogo2[1] or jogo1[0] == jogo2[2]:
-        print('voce Perdeu')
+if jogo1 == jogo2:
+    print('Empatou')
 
-else:
-    print('Opçao Errada')
+elif escolha == 0 and computador == 1 or escolha == 1 and computador == 2 or escolha == 2 and computador == 0:
+    print('voce Ganhou')
+
+elif computador == 0 and escolha == 1 or computador == 1 and escolha == 2 or computador == 2 and escolha == 0:
+    print('voce Perdeu')
